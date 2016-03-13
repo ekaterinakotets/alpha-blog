e9f26887f897b3375019ed9f6c4d65a76ab99f60
Alpha-blog <ekaterinakotets@walla.com>
2016-03-13 19:14:20 +0000

    Create show action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -26,7 +26,7 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    debugger
+    
     @article = Article.new(article_params)
     @article.user = User.first
     if @article.save
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,30 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Alpha blog #{@user.username}"
+      flash[:success] = "Welcome to Alpha blog, #{@user.username}"
       redirect_to articles_path
     else 
       render 'new'
     end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully "
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    
   end
   
   private
