a172c0de9de9413ad07ec5bfa0064eece63304aa
Alpha-blog <ekaterinakotets@walla.com>
2016-04-13 08:33:28 +0000

    update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,6 +32,7 @@ class ArticlesController < ApplicationController
     @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created."
+      redirect_to article_path(@article)
     else
       render 'new'
     end
@@ -53,7 +54,7 @@ class ArticlesController < ApplicationController
    end
    
    def article_params
-     params.require(:article).permit(:title, :description)
+     params.require(:article).permit(:title, :description, category_ids: [])
    end
    
    def require_same_user
